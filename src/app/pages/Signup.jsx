28e57ade11fb482157b36b9e18b34078f632474a
andremsgomes <andremsgomes@gmail.com>
2022-05-27 01:04:11 +0100

    Removed margin

--- src/app/pages/Signup.jsx
+++ src/app/pages/Signup.jsx
@@ -136,7 +136,7 @@ function Signup() {
   return (
     <div class="gradient-custom">
       <div class="container py-5 h-100">
-        <div className="row mx-1">
+        <div className="row">
           <div className="col-sm-12 col-md-8 col-lg-6 col-xl-5 mx-auto">
             <div class="card shadow-2-strong" style={{ borderRadius: "1rem" }}>
               <div class="card-body p-5 text-center">
