4a415dd4116d191371c667a16c1447d0a5473d72
Andre Gomes <andremsgomes@gmail.com>
2025-09-11 20:02:59 +0100

    add function to get ordinal suffix

---
+++ src/app/utils/ranking.js
@@ -0,0 +1,9 @@
+export function getOrdinalSuffix(rank) {
+  const j = rank % 10;
+  const k = rank % 100;
+
+  if (j === 1 && k !== 11) return "st";
+  if (j === 2 && k !== 12) return "nd";
+  if (j === 3 && k !== 13) return "rd";
+  return "th";
+}
