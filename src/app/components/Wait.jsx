b1e96261f766c81547b2ea3081d9db09971a61de
Andre Gomes <andremsgomes@gmail.com>
2025-09-21 17:08:16 +0100

    add translations for wait component

--- src/app/components/Wait.jsx
+++ src/app/components/Wait.jsx
@@ -1,13 +1,26 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
 
 function Wait(props) {
+  const { t } = useTranslation(undefined, { keyPrefix: "wait" });
+
   return (
     <div className="text-center page-center">
       <h2 className="mx-4">{props.title}</h2>
       <div className="mt-4">
-        {props.rank !== 0 && <h4>Estás em {props.rank}º lugar</h4>}
+        {props.rank !== 0 && (
+          <h4>
+            {t("rank", {
+              rank: props.rank,
+              suffix: getOrdinalSuffix(props.rank),
+            })}
+          </h4>
+        )}
         <h5>
-          Tens {props.points} ponto{props.points !== 1 && "s"}
+          {t("score", {
+            points: props.points,
+            suffix: props.points !== 1 ? "s" : "",
+          })}
         </h5>
       </div>
       {props.storeButton && (
@@ -15,7 +28,7 @@ function Wait(props) {
           className="btn btn-lg btn-primary mt-4"
           onClick={props.onStoreClick}
         >
-          Comprar troféus
+          {t('store-button')}
         </button>
       )}
     </div>
