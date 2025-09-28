029a6cdf1f041db4996380a7801e47861b88a904
Andre Gomes <andremsgomes@gmail.com>
2025-09-28 15:01:46 +0100

    add translations for email validation

--- src/app/utils/email-validation.js
+++ src/app/utils/email-validation.js
@@ -1,12 +1,14 @@
+import { useTranslation } from "react-i18next";
+
 export const isValidEmail = (email, setEmailErrorMessage) => {
+  const { t } = useTranslation(undefined, { keyPrefix: "email-validation" });
+
   if (email.length === 0) {
-    setEmailErrorMessage("Por favor introduz um email válido.");
+    setEmailErrorMessage(t("error-message"));
     return false;
-  } 
+  }
   if (!/\w+@\w+.\w+/.test(email)) {
-    setEmailErrorMessage(
-      "Por favor introduz um email válido."
-    );
+    setEmailErrorMessage(t("error-message"));
     return false;
   }
 
