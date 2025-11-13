a59875c063e9ff7c75856a5715f182f068e0a881
Andre Gomes <andremsgomes@gmail.com>
2025-11-13 19:18:41 +0000

    refactor edit badge page

--- src/app/pages/EditBadge.jsx
+++ src/app/pages/EditBadge.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 
 import api from "../api";
 import { Link, useParams } from "react-router-dom";
@@ -7,96 +7,49 @@ import { useTranslation } from "react-i18next";
 import EditAndLogout from "../components/EditAndLogout";
 import BadgeForm from "../components/BadgeForm";
 
-function withParams(Component) {
-  return (props) => <Component {...props} params={useParams()} />;
-}
+function EditBadge() {
+  const { board, id } = useParams();
+  const { t } = useTranslation(undefined, { keyPrefix: "edit-badge" });
 
-class EditBadge extends Component {
-  constructor(props) {
-    super(props);
+  const [name, setName] = useState("");
+  const [image, setImage] = useState("");
+  const [multiplier, setMultiplier] = useState(1);
+  const [cost, setCost] = useState(0);
 
-    this.handleNameChange = this.handleNameChange.bind(this);
-    this.handleImageChange = this.handleImageChange.bind(this);
-    this.handleMultiplierChange = this.handleMultiplierChange.bind(this);
-    this.handleCostChange = this.handleCostChange.bind(this);
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  state = {
-    name: "",
-    image: "",
-    multiplier: 1,
-    cost: 0,
-  };
-
-  componentDidMount() {
+  useEffect(() => {
     api
-      .badge(this.props.params.id)
+      .badge(id)
       .then((res) => {
-        this.setState({
-          name: res.data.name,
-          image: res.data.image,
-          multiplier: res.data.multiplier,
-          cost: res.data.cost,
-        });
+        setName(res.data.name);
+        setImage(res.data.image);
+        setMultiplier(res.data.multiplier);
+        setCost(res.data.cost);
       })
       .catch((error) => {
         console.log(error.message);
       });
-  }
+  }, [id]);
 
-  handleNameChange(e) {
-    this.setState({
-      name: e.target.value,
-    });
-  }
-
-  handleImageChange(e) {
-    this.setState({
-      image: e.target.value,
-    });
-  }
-
-  handleMultiplierChange(e) {
-    this.setState({
-      multiplier: parseInt(e.target.value),
-    });
-  }
-
-  handleCostChange(e) {
-    this.setState({
-      cost: parseInt(e.target.value),
-    });
-  }
-
-  handleClick() {
+  const handleClick = () => {
     // TODO: validar tudo
 
-    const id = this.props.params.id;
-    const name = this.state.name;
-    const multiplier = this.state.multiplier;
-    const cost = this.state.cost;
-
     // image validation
-    const image =
-      this.state.image.length > 0
-        ? this.state.image
+    const finalImage =
+      image.length > 0
+        ? image
         : "https://www.linkpicture.com/q/badge_9.png";
 
-    const payload = { id, name, image, multiplier, cost };
+    const payload = { id, name, image: finalImage, multiplier, cost };
 
     api
       .updateBadge(payload)
       .then(() => {
-        window.location.href = `/admin/${this.props.params.board}/edit`;
+        window.location.href = `/admin/${board}/edit`;
       })
       .catch((error) => {
         console.log(error.message);
       });
   }
-
-  render() {
-    const { t } = useTranslation(undefined, { keyPrefix: "edit-badge" });
 
     return (
       <div>
@@ -112,15 +65,15 @@ class EditBadge extends Component {
             </li>
             <li className="breadcrumb-item" aria-current="page">
               <Link
-                to={`/admin/${this.props.params.board}`}
+                to={`/admin/${board}`}
                 className="text-decoration-none"
               >
-                {this.props.params.board}
+                {board}
               </Link>
             </li>
             <li className="breadcrumb-item" aria-current="page">
               <Link
-                to={`/admin/${this.props.params.board}/edit`}
+                to={`/admin/${board}/edit`}
                 className="text-decoration-none"
               >
                 {t('breadcrumbs.edit')}
@@ -138,27 +91,26 @@ class EditBadge extends Component {
           </div>
         </nav>
         <div className="text-center mt-5">
-          <h1>{this.props.params.board}</h1>
+          <h1>{board}</h1>
           <div className="card my-5 mx-md-5 py-2 px-0">
             <div className="card-body px-0">
               <h3 className="card-title">{t('title')}</h3>
               <BadgeForm
-                onNameChange={this.handleNameChange}
-                name={this.state.name}
-                onImageChange={this.handleImageChange}
-                image={this.state.image}
-                onMultiplierChange={this.handleMultiplierChange}
-                multiplier={this.state.multiplier}
-                onCostChange={this.handleCostChange}
-                cost={this.state.cost}
-                onClick={this.handleClick}
+                onNameChange={setName}
+                name={name}
+                onImageChange={setImage}
+                image={image}
+                onMultiplierChange={multiplier}
+                multiplier={multiplier}
+                onCostChange={setCost}
+                cost={cost}
+                onClick={handleClick}
               />
             </div>
           </div>
         </div>
       </div>
     );
-  }
 }
 
-export default withParams(EditBadge);
+export default EditBadge;
