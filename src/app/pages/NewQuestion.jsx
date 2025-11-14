5576b09a5292929ac0c5235715b155e3cfab1df3
Andre Gomes <andremsgomes@gmail.com>
2025-11-14 23:27:15 +0000

    refactor new question page

--- src/app/pages/NewQuestion.jsx
+++ src/app/pages/NewQuestion.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 
 import api from "../api";
 import { Link, useParams } from "react-router-dom";
@@ -7,169 +7,144 @@ import { useTranslation } from "react-i18next";
 import QuestionForm from "../components/QuestionForm";
 import EditAndLogout from "../components/EditAndLogout";
 
-function withParams(Component) {
-  return (props) => <Component {...props} params={useParams()} />;
-}
+function NewQuestion() {
+  const { t } = useTranslation(undefined, { keyPrefix: "new-question" });
+  const { board, tile } = useParams();
 
-class NewQuestion extends Component {
-  constructor(props) {
-    super(props);
+  const [question, setQuestion] = useState("");
+  const [image, setImage] = useState(null);
+  const [preview, setPreview] = useState("");
+  const [answers, setAnswers] = useState([
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+  ]);
+  const [correctAnswer, setCorrectAnswer] = useState(1);
 
-    this.handleQuestionChange = this.handleQuestionChange.bind(this);
-    this.handleImageChange = this.handleImageChange.bind(this);
-    this.handleAnswerChange = this.handleAnswerChange.bind(this);
-    this.handleSelectChange = this.handleSelectChange.bind(this);
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  state = {
-    question: "",
-    image: null,
-    preview: "",
-    answers: ["", "", "", "", "", "", "", "", "", ""],
-    correctAnswer: 1,
+  const handleImageChange = (files) => {
+    const imageFile = files[0];
+    setImage(imageFile);
+    setPreview(URL.createObjectURL(imageFile));
   };
 
-  handleQuestionChange(e) {
-    this.setState({
-      question: e.target.value,
-    });
-  }
-
-  handleImageChange(files) {
-    const image = files[0];
-    this.setState({ image, preview: URL.createObjectURL(image) });
-  }
-
-  handleAnswerChange(e, i) {
-    let newAnswers = [...this.state.answers];
+  const handleAnswerChange = (e, i) => {
+    let newAnswers = [...answers];
     newAnswers[i] = e.target.value;
 
-    this.setState({
-      answers: newAnswers,
-    });
-  }
+    setAnswers(newAnswers);
+  };
 
-  handleSelectChange(e) {
-    this.setState({
-      correctAnswer: parseInt(e.target.value),
-    });
-  }
+  const handleSelectChange = (e) => {
+    setCorrectAnswer(parseInt(e.target.value));
+  };
 
-  async handleClick() {
+  const handleClick = async () => {
     // TODO: validar tudo
-    const boardName = this.props.params.board;
-    const boardPosition = parseInt(this.props.params.tile);
-    const question = this.state.question;
-    const image = this.state.image;
-    let answers = [];
-    const correctAnswer = this.state.correctAnswer;
+    const boardPosition = parseInt(tile);
+    let finalAnswers = [];
 
-    this.state.answers.forEach((answer) => {
-      if (answer.length > 0) answers.push(answer);
+    answers.forEach((answer) => {
+      if (answer.length > 0) finalAnswers.push(answer);
     });
 
     const payload = new FormData();
 
-    if (image && this.state.preview.length > 0) {
-      const response = await fetch(this.state.preview);
+    if (image && preview.length > 0) {
+      const response = await fetch(preview);
       const blob = await response.blob();
       payload.append("image", blob, image.name);
     }
 
-    payload.append("boardName", boardName);
+    payload.append("boardName", board);
     payload.append("boardPosition", boardPosition);
     payload.append("question", question);
-    payload.append("answers", JSON.stringify(answers));
+    payload.append("answers", JSON.stringify(finalAnswers));
     payload.append("correctAnswer", correctAnswer);
 
     api
       .newQuestion(payload)
       .then(() => {
-        window.location.href = `/admin/${this.props.params.board}/${this.props.params.tile}/questions`;
+        window.location.href = `/admin/${board}/${tile}/questions`;
       })
       .catch((error) => {
         console.log(error.message);
       });
-  }
+  };
 
-  render() {
-    const { t } = useTranslation(undefined, { keyPrefix: "new-question" });
-
-    return (
-      <div>
-        <nav
-          aria-label="breadcrumb"
-          className="navbar navbar-light bg-white px-4"
-        >
-          <ol className="breadcrumb m-0">
-            <li className="breadcrumb-item" aria-current="page">
-              <Link to="/admin" className="text-decoration-none">
-                {t("breadcrumbs.menu")}
-              </Link>
-            </li>
-            <li className="breadcrumb-item" aria-current="page">
-              <Link
-                to={`/admin/${this.props.params.board}`}
-                className="text-decoration-none"
-              >
-                {this.props.params.board}
-              </Link>
-            </li>
-            <li className="breadcrumb-item" aria-current="page">
-              <Link
-                to={`/admin/${this.props.params.board}/edit`}
-                className="text-decoration-none"
-              >
-                {t("breadcrumbs.edit")}
-              </Link>
-            </li>
-            <li className="breadcrumb-item" aria-current="page">
-              {t("breadcrumbs.tile", { tile: this.props.params.tile })}
-            </li>
-            <li className="breadcrumb-item" aria-current="page">
-              <Link
-                to={`/admin/${this.props.params.board}/${this.props.params.tile}/questions`}
-                className="text-decoration-none"
-              >
-                {t("breadcrumbs.questions")}
-              </Link>
-            </li>
-            <li className="breadcrumb-item active" aria-current="page">
-              {t("breadcrumbs.new-question")}
-            </li>
-          </ol>
-          <div>
-            <EditAndLogout />
-          </div>
-        </nav>
-        <div className="text-center mt-5">
-          <h1>
-            {t("title", {
-              board: this.props.params.board,
-              tile: this.props.params.tile,
-            })}
-          </h1>
-          <div className="card my-5 mx-md-5 py-2 px-0">
-            <div className="card-body px-0">
-              <h3 className="card-title">{t("breadcrumbs.subtitle")}</h3>
-              <QuestionForm
-                onQuestionChange={this.handleQuestionChange}
-                question={this.state.question}
-                onImageChange={this.handleImageChange}
-                preview={this.state.preview}
-                answers={this.state.answers}
-                onAnswerChange={this.handleAnswerChange}
-                correctAnswer={this.state.correctAnswer}
-                onSelectChange={this.handleSelectChange}
-                onClick={this.handleClick}
-              />
-            </div>
+  return (
+    <div>
+      <nav
+        aria-label="breadcrumb"
+        className="navbar navbar-light bg-white px-4"
+      >
+        <ol className="breadcrumb m-0">
+          <li className="breadcrumb-item" aria-current="page">
+            <Link to="/admin" className="text-decoration-none">
+              {t("breadcrumbs.menu")}
+            </Link>
+          </li>
+          <li className="breadcrumb-item" aria-current="page">
+            <Link to={`/admin/${board}`} className="text-decoration-none">
+              {board}
+            </Link>
+          </li>
+          <li className="breadcrumb-item" aria-current="page">
+            <Link to={`/admin/${board}/edit`} className="text-decoration-none">
+              {t("breadcrumbs.edit")}
+            </Link>
+          </li>
+          <li className="breadcrumb-item" aria-current="page">
+            {t("breadcrumbs.tile", { tile: tile })}
+          </li>
+          <li className="breadcrumb-item" aria-current="page">
+            <Link
+              to={`/admin/${board}/${tile}/questions`}
+              className="text-decoration-none"
+            >
+              {t("breadcrumbs.questions")}
+            </Link>
+          </li>
+          <li className="breadcrumb-item active" aria-current="page">
+            {t("breadcrumbs.new-question")}
+          </li>
+        </ol>
+        <div>
+          <EditAndLogout />
+        </div>
+      </nav>
+      <div className="text-center mt-5">
+        <h1>
+          {t("title", {
+            board: board,
+            tile: tile,
+          })}
+        </h1>
+        <div className="card my-5 mx-md-5 py-2 px-0">
+          <div className="card-body px-0">
+            <h3 className="card-title">{t("subtitle")}</h3>
+            <QuestionForm
+              onQuestionChange={setQuestion}
+              question={question}
+              onImageChange={handleImageChange}
+              preview={preview}
+              answers={answers}
+              onAnswerChange={handleAnswerChange}
+              correctAnswer={correctAnswer}
+              onSelectChange={handleSelectChange}
+              onClick={handleClick}
+            />
           </div>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
-export default withParams(NewQuestion);
+export default NewQuestion;
