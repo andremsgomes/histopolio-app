892b128433a06682e64e3d09539121d9d8aaa0a1
Andre Gomes <andremsgomes@gmail.com>
2025-11-16 16:24:32 +0000

    refactor edit deck cards page

--- src/app/pages/EditDeckCards.jsx
+++ src/app/pages/EditDeckCards.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 
 import api from "../api";
 import EditAndLogout from "../components/EditAndLogout";
@@ -9,157 +9,143 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPencil, faTrashCan } from "@fortawesome/free-solid-svg-icons";
 import { useTranslation } from "react-i18next";
 
-function withParams(Component) {
-  return (props) => <Component {...props} params={useParams()} />;
-}
+function EditDeckCards() {
+  const { board, deck } = useParams();
+  const { t } = useTranslation(undefined, { keyPrefix: "edit-deck-cards" });
 
-class EditDeckCards extends Component {
-  state = {
-    cards: [],
-  };
+  const [cards, setCards] = useState([]);
 
-  componentDidMount() {
+  useEffect(() => {
     api
-      .deckCards(this.props.params.board, this.props.params.deck)
+      .deckCards(board, deck)
       .then((res) => {
-        this.setState({
-          cards: res.data,
-        });
+        setCards(res.data);
       })
       .catch((error) => {
         console.log(error.message);
       });
-  }
+  }, [board, deck]);
 
-  handleDelete(id) {
+  const handleDelete = (id) => {
     api.deleteCard(id).then(() => {
-      const newCards = this.state.cards.filter((card) => {
+      const newCards = cards.filter((card) => {
         return card._id !== id;
       });
 
-      this.setState({
-        cards: newCards,
-      });
+      setCards(newCards);
     });
-  }
+  };
 
-  render() {
-    const { t } = useTranslation(undefined, { keyPrefix: "edit-deck-cards" });
-
-    return (
-      <div>
-        <nav
-          aria-label="breadcrumb"
-          className="navbar navbar-light bg-white px-4"
-        >
-          <ol className="breadcrumb m-0">
-            <li className="breadcrumb-item" aria-current="page">
-              <Link to="/admin" className="text-decoration-none">
-                {t("breadcrumbs.menu")}
-              </Link>
-            </li>
-            <li className="breadcrumb-item" aria-current="page">
-              <Link
-                to={`/admin/${this.props.params.board}`}
-                className="text-decoration-none"
-              >
-                {this.props.params.board}
-              </Link>
-            </li>
-            <li className="breadcrumb-item" aria-current="page">
-              {t("breadcrumbs.cards")}
-            </li>
-            <li className="breadcrumb-item active" aria-current="page">
-              {this.props.params.deck === "chance"
-                ? t("breadcrumbs.chance")
-                : t("breadcrumbs.senate-decisions")}
-            </li>
-          </ol>
-          <div>
-            <EditAndLogout />
-          </div>
-        </nav>
-        <div className="text-center mt-5">
-          <h1>{this.props.params.board}</h1>
-          <div className="card my-5 mx-md-5 py-2 px-0">
-            <div className="card-body px-0">
-              <h3 className="card-title">
-                {this.props.params.deck === "chance"
-                  ? t("title.chance")
-                  : t("title.senate-decisions")}
-              </h3>
-              <div className="table-responsive mt-3">
-                <table className="table table-hover">
-                  <thead>
-                    <tr>
-                      <th scope="col">#</th>
-                      <th scope="col">{t("table.description-column.name")}</th>
-                      <th scope="col">
-                        {t("table.immediate-points-column.name")}
-                      </th>
-                      <th scope="col">{t("table.action-column.name")}</th>
-                      <th scope="col"></th>
-                      <th scope="col"></th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {this.state.cards.map((card, i) => {
-                      return (
-                        <tr>
-                          <th scope="row">{i + 1}</th>
-                          <td>{card.info}</td>
-                          <td>{card.points}</td>
-                          <td>
-                            {card.action === "none" ? (
-                              t("table.action-column.options.no-action")
-                            ) : (
-                              <>
-                                {card.action === "move"
-                                  ? t("table.action-column.options.move", {
-                                      count: card.actionValue,
-                                    })
-                                  : t("table.action-column.options.move-to", {
-                                      tile: card.actionValue,
-                                    })}
-                              </>
-                            )}
-                          </td>
-                          <td>
-                            <Link
-                              to={`/admin/${this.props.params.board}/cards/deck/${card._id}/edit`}
-                            >
-                              <FontAwesomeIcon icon={faPencil} />
-                            </Link>
-                          </td>
-                          <td>
-                            <Link
-                              to="#"
-                              className="text-danger"
-                              onClick={() => this.handleDelete(card._id)}
-                            >
-                              <FontAwesomeIcon icon={faTrashCan} />
-                            </Link>
-                          </td>
-                        </tr>
-                      );
-                    })}
-                  </tbody>
-                </table>
-              </div>
-              <Link
-                to={"/admin/" + this.props.params.board + "/cards/deck/new"}
-                className="text-decoration-none"
-              >
-                <button className="btn btn-lg btn-primary mt-3">
-                  {t("add-button")}
-                </button>
-              </Link>
+  return (
+    <div>
+      <nav
+        aria-label="breadcrumb"
+        className="navbar navbar-light bg-white px-4"
+      >
+        <ol className="breadcrumb m-0">
+          <li className="breadcrumb-item" aria-current="page">
+            <Link to="/admin" className="text-decoration-none">
+              {t("breadcrumbs.menu")}
+            </Link>
+          </li>
+          <li className="breadcrumb-item" aria-current="page">
+            <Link to={`/admin/${board}`} className="text-decoration-none">
+              {board}
+            </Link>
+          </li>
+          <li className="breadcrumb-item" aria-current="page">
+            {t("breadcrumbs.cards")}
+          </li>
+          <li className="breadcrumb-item active" aria-current="page">
+            {deck === "chance"
+              ? t("breadcrumbs.chance")
+              : t("breadcrumbs.senate-decisions")}
+          </li>
+        </ol>
+        <div>
+          <EditAndLogout />
+        </div>
+      </nav>
+      <div className="text-center mt-5">
+        <h1>{board}</h1>
+        <div className="card my-5 mx-md-5 py-2 px-0">
+          <div className="card-body px-0">
+            <h3 className="card-title">
+              {deck === "chance"
+                ? t("title.chance")
+                : t("title.senate-decisions")}
+            </h3>
+            <div className="table-responsive mt-3">
+              <table className="table table-hover">
+                <thead>
+                  <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">{t("table.description-column.name")}</th>
+                    <th scope="col">
+                      {t("table.immediate-points-column.name")}
+                    </th>
+                    <th scope="col">{t("table.action-column.name")}</th>
+                    <th scope="col"></th>
+                    <th scope="col"></th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {cards.map((card, i) => {
+                    return (
+                      <tr>
+                        <th scope="row">{i + 1}</th>
+                        <td>{card.info}</td>
+                        <td>{card.points}</td>
+                        <td>
+                          {card.action === "none" ? (
+                            t("table.action-column.options.no-action")
+                          ) : (
+                            <>
+                              {card.action === "move"
+                                ? t("table.action-column.options.move", {
+                                    count: card.actionValue,
+                                  })
+                                : t("table.action-column.options.move-to", {
+                                    tile: card.actionValue,
+                                  })}
+                            </>
+                          )}
+                        </td>
+                        <td>
+                          <Link
+                            to={`/admin/${board}/cards/deck/${card._id}/edit`}
+                          >
+                            <FontAwesomeIcon icon={faPencil} />
+                          </Link>
+                        </td>
+                        <td>
+                          <Link
+                            to="#"
+                            className="text-danger"
+                            onClick={() => handleDelete(card._id)}
+                          >
+                            <FontAwesomeIcon icon={faTrashCan} />
+                          </Link>
+                        </td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </table>
             </div>
+            <Link
+              to={"/admin/" + board + "/cards/deck/new"}
+              className="text-decoration-none"
+            >
+              <button className="btn btn-lg btn-primary mt-3">
+                {t("add-button")}
+              </button>
+            </Link>
           </div>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
-export default withParams(EditDeckCards);
+export default EditDeckCards;
