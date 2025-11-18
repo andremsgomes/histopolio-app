46c93031022aae944158c9f323636d2e54da9dd2
Andre Gomes <andremsgomes@gmail.com>
2025-11-18 18:04:11 +0000

    refactor new train card page

--- src/app/pages/NewTrainCard.jsx
+++ src/app/pages/NewTrainCard.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 
 import api from "../api";
 import { Link, useParams } from "react-router-dom";
@@ -7,129 +7,93 @@ import { useTranslation } from "react-i18next";
 import EditAndLogout from "../components/EditAndLogout";
 import TrainCardForm from "../components/TrainCardForm";
 
-function withParams(Component) {
-  return (props) => <Component {...props} params={useParams()} />;
-}
+function NewTrainCard() {
+  const { t } = useTranslation(undefined, { keyPrefix: "new-train-card" });
+  const { board, tile } = useParams();
 
-class NewTrainCard extends Component {
-  constructor(props) {
-    super(props);
+  const [info, setInfo] = useState("");
+  const [content, setContent] = useState("");
 
-    this.handleInfoChange = this.handleInfoChange.bind(this);
-    this.handleContentChange = this.handleContentChange.bind(this);
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  state = {
-    info: "",
-    content: "",
-  };
-
-  handleInfoChange(e) {
-    this.setState({
-      info: e.target.value,
-    });
-  }
-
-  handleContentChange(e) {
-    this.setState({
-      content: e.target.value,
-    });
-  }
-
-  handleClick() {
+  const handleClick = () => {
     // TODO: validar tudo
 
-    const boardName = this.props.params.board;
-    const boardPosition = parseInt(this.props.params.tile);
-    const info = this.state.info;
-    const content = this.state.content;
+    const boardPosition = parseInt(tile);
 
-    const payload = { boardName, boardPosition, info, content };
+    const payload = { boardName: board, boardPosition, info, content };
 
     api
       .newTrainCard(payload)
       .then(() => {
-        window.location.href = `/admin/${this.props.params.board}/${this.props.params.tile}/train_cards`;
+        window.location.href = `/admin/${board}/${tile}/train_cards`;
       })
       .catch((error) => {
         console.log(error.message);
       });
-  }
+  };
 
-  render() {
-    const { t } = useTranslation(undefined, { keyPrefix: "new-train-card" });
-
-    return (
-      <div>
-        <nav
-          aria-label="breadcrumb"
-          className="navbar navbar-light bg-white px-4"
-        >
-          <ol className="breadcrumb m-0">
-            <li className="breadcrumb-item" aria-current="page">
-              <Link to="/admin" className="text-decoration-none">
-                {t("breadcrumbs.menu")}
-              </Link>
-            </li>
-            <li className="breadcrumb-item" aria-current="page">
-              <Link
-                to={`/admin/${this.props.params.board}`}
-                className="text-decoration-none"
-              >
-                {this.props.params.board}
-              </Link>
-            </li>
-            <li className="breadcrumb-item" aria-current="page">
-              <Link
-                to={`/admin/${this.props.params.board}/edit`}
-                className="text-decoration-none"
-              >
-                {t("breadcrumbs.edit")}
-              </Link>
-            </li>
-            <li className="breadcrumb-item" aria-current="page">
-              {t("breadcrumbs.tile", { tile: this.props.params.tile })}
-            </li>
-            <li className="breadcrumb-item" aria-current="page">
-              <Link
-                to={`/admin/${this.props.params.board}/${this.props.params.tile}/train_cards`}
-                className="text-decoration-none"
-              >
-                {t("breadcrumbs.train-cards")}
-              </Link>
-            </li>
-            <li className="breadcrumb-item active" aria-current="page">
-              {t("breadcrumbs.new-card")}
-            </li>
-          </ol>
-          <div>
-            <EditAndLogout />
-          </div>
-        </nav>
-        <div className="text-center mt-5">
-          <h1>
-            {t("title", {
-              board: this.props.params.board,
-              tile: this.props.params.tile,
-            })}
-          </h1>
-          <div className="card my-5 mx-md-5 py-2 px-0">
-            <div className="card-body px-0">
-              <h3 className="card-title">{t("subtitle")}</h3>
-              <TrainCardForm
-                onInfoChange={this.handleInfoChange}
-                info={this.state.info}
-                onContentChange={this.handleContentChange}
-                content={this.state.content}
-                onClick={this.handleClick}
-              />
-            </div>
+  return (
+    <div>
+      <nav
+        aria-label="breadcrumb"
+        className="navbar navbar-light bg-white px-4"
+      >
+        <ol className="breadcrumb m-0">
+          <li className="breadcrumb-item" aria-current="page">
+            <Link to="/admin" className="text-decoration-none">
+              {t("breadcrumbs.menu")}
+            </Link>
+          </li>
+          <li className="breadcrumb-item" aria-current="page">
+            <Link to={`/admin/${board}`} className="text-decoration-none">
+              {board}
+            </Link>
+          </li>
+          <li className="breadcrumb-item" aria-current="page">
+            <Link to={`/admin/${board}/edit`} className="text-decoration-none">
+              {t("breadcrumbs.edit")}
+            </Link>
+          </li>
+          <li className="breadcrumb-item" aria-current="page">
+            {t("breadcrumbs.tile", { tile: tile })}
+          </li>
+          <li className="breadcrumb-item" aria-current="page">
+            <Link
+              to={`/admin/${board}/${tile}/train_cards`}
+              className="text-decoration-none"
+            >
+              {t("breadcrumbs.train-cards")}
+            </Link>
+          </li>
+          <li className="breadcrumb-item active" aria-current="page">
+            {t("breadcrumbs.new-card")}
+          </li>
+        </ol>
+        <div>
+          <EditAndLogout />
+        </div>
+      </nav>
+      <div className="text-center mt-5">
+        <h1>
+          {t("title", {
+            board: board,
+            tile: tile,
+          })}
+        </h1>
+        <div className="card my-5 mx-md-5 py-2 px-0">
+          <div className="card-body px-0">
+            <h3 className="card-title">{t("subtitle")}</h3>
+            <TrainCardForm
+              onInfoChange={setInfo}
+              info={info}
+              onContentChange={setContent}
+              content={content}
+              onClick={handleClick}
+            />
           </div>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
-export default withParams(NewTrainCard);
+export default NewTrainCard;
