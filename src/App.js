134b03ff3b85ca469f505481aa78c9ca4446f1ae
Andre Gomes <andremsgomes@gmail.com>
2026-02-28 15:42:31 +0000

    use pt

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import React from "react";
 
 import RootNavigator from "./app/navigation/RootNavigator";
 
+import i18n from "./i18n";
+
 function App() {
+  i18n.changeLanguage("pt");
   return (
     <RootNavigator />
   );
